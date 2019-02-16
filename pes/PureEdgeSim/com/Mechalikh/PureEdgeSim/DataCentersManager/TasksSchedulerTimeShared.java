faa419ce1a617aee35514987fd57aeba570bc052
CharafeddineMechalikh <46229052+CharafeddineMechalikh@users.noreply.github.com>
2019-02-16 11:48:01 +0100

    Update TasksSchedulerTimeShared.java

--- pes/PureEdgeSim/com/Mechalikh/PureEdgeSim/DataCentersManager/TasksSchedulerTimeShared.java
+++ pes/PureEdgeSim/com/Mechalikh/PureEdgeSim/DataCentersManager/TasksSchedulerTimeShared.java
@@ -18,35 +18,36 @@ public class TasksSchedulerTimeShared extends CloudletSchedulerTimeShared {
 		// TODO Auto-generated constructor stub
 	} 
 		@Override
-		public void cloudletFinish(final CloudletExecution ce) {
-			Task task = ((Task) ce.getCloudlet());
-			EdgeDataCenter edc = (EdgeDataCenter) task.getVm().getHost().getDatacenter();
-			
-			if (edc.isDead() || task.getEdgeDevice().isDead()) { 
-				//the destination (where the task is executed) 
-				//or the origin of the task(the device which offloaded the task)
-	           // if one of them is dead
-				task.setFailureReason(Status.FAILED_BECAUSE_DEVICE_DEAD);
-				ce.setCloudletStatus(Cloudlet.Status.FAILED);
-			} 
-		 
-			
-			else
-			// a simple representation of task failure due to device mobility, if there is
-			// no vm migration
-			// if vm location doesn't equal the edge device location (that generated this
-			// task)
-			if (edc.getType()!=SimulationParameters.TYPES.CLOUD && !edc.getLocation().equals(task.getEdgeDevice().getLocation())) {
-				task.setFailureReason(Task.Status.FAILED_DUE_TO_DEVICE_MOBILITY);
-				ce.setCloudletStatus(Cloudlet.Status.FAILED);
-			} else {
-			  ce.setCloudletStatus(Cloudlet.Status.SUCCESS);
-			  task.getEdgeDevice().addConsumption(task.getOutputSize()*SimulationParameters.POWER_CONS_PER_MEGABYTE);
-			  edc.addConsumption(task.getOutputSize()*SimulationParameters.POWER_CONS_PER_MEGABYTE);
-			}
-			ce.finalizeCloudlet();
-			addCloudletToFinishedList(ce);
+	public void cloudletFinish(final CloudletExecution ce) {
+		Task task = ((Task) ce.getCloudlet());
+		EdgeDataCenter edc = (EdgeDataCenter) task.getVm().getHost().getDatacenter();
+		// task failed long delay
+		if ((task.getSimulation().clock() - task.getTime()) + task.getUploadLanNetworkUsage() > task.getMaxLatency()) {
+			task.setFailureReason(Task.Status.FAILED_DUE_TO_LATENCY);
+			task.setStatus(Cloudlet.Status.FAILED);		  
+		} 
+		else
+		if (edc.isDead() || task.getEdgeDevice().isDead()) { 
+			//the destination (where the task is executed) 
+			//or the origin of the task(the device which offloaded the task)
+           // if one of them is dead
+			task.setFailureReason(Status.FAILED_BECAUSE_DEVICE_DEAD);
+			ce.setCloudletStatus(Cloudlet.Status.FAILED);
+		} 	 
+		else
+		// a simple representation of task failure due to device mobility, if there is
+		// no vm migration
+		// if vm location doesn't equal the edge device location (that generated this
+		// task)
+		if (edc.getType()!=SimulationParameters.TYPES.CLOUD && !edc.getLocation().equals(task.getEdgeDevice().getLocation())) {
+			task.setFailureReason(Task.Status.FAILED_DUE_TO_DEVICE_MOBILITY);
+			ce.setCloudletStatus(Cloudlet.Status.FAILED);
+		} else {
+		  ce.setCloudletStatus(Cloudlet.Status.SUCCESS);
 		}
+		ce.finalizeCloudlet();
+		addCloudletToFinishedList(ce);
+	}
 
 
 
